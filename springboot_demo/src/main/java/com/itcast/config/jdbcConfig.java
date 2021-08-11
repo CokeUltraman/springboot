7b529323f0bb2cdfbe881712fd46124c708ea477
cokeultraman <tonyzeng12@gmail.com>
2021-08-11 01:58:23 -0400

    yml

---
+++ springboot_demo/src/main/java/com/itcast/config/jdbcConfig.java
@@ -0,0 +1,52 @@
+package com.itcast.config;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+import javax.sql.DataSource;
+
+@Configuration
+//@PropertySource("classpath:application.properties")
+//@EnableConfigurationProperties(JdbcProperties.class)
+public class jdbcConfig {
+//    @Value("${jdbc.url}")
+//    String url;
+//    @Value("${jdbc.driver}")
+//    String driver;
+//    @Value("${jdbc.username}")
+//    String username;
+//    @Value("${jdbc.password}")
+//    String password;
+//
+//    @Bean
+//    public DataSource dataSource(){
+//        DruidDataSource dataSource=new DruidDataSource();
+//        dataSource.setDriverClassName(driver);
+//        dataSource.setUsername(username);
+//        dataSource.setPassword(password);
+//        dataSource.setUrl(url);
+//        return dataSource;
+//    }
+//    @Bean
+//    public DataSource dataSource(JdbcProperties jdbcProperties){
+//        DruidDataSource dataSource=new DruidDataSource();
+//        dataSource.setDriverClassName(jdbcProperties.getDriver());
+//        dataSource.setUsername(jdbcProperties.getUsername());
+//        dataSource.setPassword(jdbcProperties.getPassword());
+//        dataSource.setUrl(jdbcProperties.getUrl());
+//        return dataSource;
+//    }
+    @Bean
+    @ConfigurationProperties(prefix = "jdbc")
+    public DataSource dataSource(){
+        return new DruidDataSource();
+
+    }
+
+
+}
